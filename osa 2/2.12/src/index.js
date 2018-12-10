3af3107eb794278aea0e671e7855b599eef33e81
k0tix <otto.pirttimaa@gmail.com>
2018-12-10 02:08:11 +0200

    tehtävä 2.13

--- osa 2/2.12/src/index.js
+++ osa 2/2.12/src/index.js
@@ -27,49 +27,48 @@ class App extends React.Component {
     }
 
     render() {
+        const CountryNames = ({ countries, filter }) => {
+            const filteredCountries = countries.filter(country => country.name.toLowerCase().includes(filter.toLowerCase()))
+            if(filteredCountries.length === 1) {
+                const country = filteredCountries[0]
+                return(
+                    <div>
+                        <Country name={country.name} capital={country.capital} population={country.population} flag={country.flag}/>
+                    </div>
+                )
+            } else if (filteredCountries.length > 10) {
+                return (
+                    <div>
+                        <p>too many matches, specify another filter</p>
+                    </div>
+                )
+            } else {
+                return (
+                    <div>
+                        {filteredCountries.map(country => <div onClick={() => this.setState({filter: country.name})} key={country.name}> {country.name} </div>)}
+                    </div>
+                )
+            }
+        }
+        
+        const Country = ({name, capital, population, flag}) => {
+            return (
+                <div>
+                    <h1>{name}</h1>
+                    <p>capital: {capital}</p>
+                    <p>population: {population} </p>
+                    <img width={300} height={200} src={flag} alt={name}/>
+                </div>
+            )
+        }
+
         return (
             <div>
-                find countries: <input onChange={this.handleFilterChange} />
+                find countries: <input onChange={this.handleFilterChange} value={this.state.filter} />
                 <CountryNames countries={this.state.countries} filter={this.state.filter} />
             </div>
-        )
+        ) 
     }
-    
-}
-
-const CountryNames = ({ countries, filter }) => {
-    const filteredCountries = countries.filter(country => country.name.toLowerCase().includes(filter.toLowerCase()))
-    if(filteredCountries.length === 1) {
-        const country = filteredCountries[0]
-        return(
-            <div>
-                <Country name={country.name} capital={country.capital} population={country.population} flag={country.flag}/>
-            </div>
-        )
-    } else if (filteredCountries.length > 10) {
-        return (
-            <div>
-                <p>too many matches, specify another filter</p>
-            </div>
-        )
-    } else {
-        return (
-            <div>
-                {filteredCountries.map(country => <div key={country.name}> {country.name} </div>)}
-            </div>
-        )
-    }
-}
-
-const Country = ({name, capital, population, flag}) => {
-    return (
-        <div>
-            <h1>{name}</h1>
-            <p>capital: {capital}</p>
-            <p>population: {population} </p>
-            <img width={300} height={200} src={flag} alt={name}/>
-        </div>
-    )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
